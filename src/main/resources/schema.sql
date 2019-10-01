c677071580ece942b361de22f0503f03397267c3
U0I2747 <U0I2747 tiago.medice@gft.com>
2019-10-01 12:22:32 +0200

    ref

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,8 @@
-#
-# In your Quartz properties file, you'll need to set
-# org.quartz.jobStore.driverDelegateClass = org.quartz.impl.jdbcjobstore.StdJDBCDelegate
+CREATE DATABASE IF NOT EXISTS QUARTZ;
+
+USE QUARTZ;
+
+
 
 DROP TABLE IF EXISTS QRTZ_FIRED_TRIGGERS;
 DROP TABLE IF EXISTS QRTZ_PAUSED_TRIGGER_GRPS;
